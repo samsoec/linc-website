8e91b14a8684ad13f49ee88e9beddba75f3adbac
Rizal Fauzy <muhammad.rizal@ruangguru.com>
2025-12-23 04:18:54 +0700

    feat: init frontend pages

---
+++ frontend/src/app/[lang]/page.tsx
@@ -0,0 +1,26 @@
+import LangRedirect from './components/LangRedirect';
+import componentResolver from './utils/component-resolver';
+import {getPageBySlug} from "@/app/[lang]/utils/get-page-by-slug";
+
+
+export default async function RootRoute({params}: { params: Promise<{ lang: string }> }) {
+    try {
+      const { lang } = await params;
+      const page = await getPageBySlug('home', lang)
+      console.log('Page data:', page);
+      if (page.error && page.error.status == 401)
+        throw new Error(
+          'Missing or invalid credentials. Have you created an access token using the Strapi admin panel? http://localhost:1337/admin/'
+        )
+
+      if (page.data.length == 0 && lang !== 'en') return <LangRedirect />
+      if (page.data.length === 0) return null
+      const contentSections = page.data[0].attributes.contentSections
+      return contentSections.map((section: any, index: number) =>
+        componentResolver(section, index)
+      )
+    } catch (error: any) {
+      console.error('Page error:', error);
+      return <div>Error loading page. Please check your credentials.</div>
+    }
+}
